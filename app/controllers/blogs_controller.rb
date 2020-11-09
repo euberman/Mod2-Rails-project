6dcdc7eb48ba3505dfba3bfa68029b838f1fa339
Gabbi Nguyen <gabbinguyen@gmail.com>
2020-11-09 16:37:19 -0600

    show/new pages

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -8,9 +8,14 @@ class BlogsController < ApplicationController
   end
 
   def new
+    @blog = Blog.new
   end
 
   def create
+    @student = Student.find(params[:id])
+    @blog = Blog.new(blog_params)
+    @blog.save
+    redirect_to student_path(@student)
   end
 
   def edit
@@ -21,4 +26,10 @@ class BlogsController < ApplicationController
 
   def delete
   end
+
+  private 
+    def blog_params
+      params.require(:blog).permit(:date, :name, :content)
+    end
+
 end
