9a15e92ac402d5d895cf880f23a138e4e480ba94
euberman <euberman@gmail.com>
2020-11-10 15:52:47 -0600

    added addEmail migration and updated seed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,13 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
   layout "main"
-  
-  def current_user
-    @current_user ||= User.find_by(id: session[:user])
+
+  def current_student
+    @current_student ||= Student.find_by(id: session[:student])
   end
 
   def logged_in?
-    current_user != nil
+    current_student != nil
   end
 
   # <% if logged_in? -%>Welcome, <%= current_user.name %><% end -%>
