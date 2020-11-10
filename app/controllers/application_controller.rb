156d6753a6407a8f7860b8fa06ec11402edf73f5
euberman <euberman@gmail.com>
2020-11-10 10:37:05 -0600

    added current_user helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user])
+  end
+
+  def logged_in?
+    current_user != nil
+  end
+
+  # <% if logged_in? -%>Welcome, <%= current_user.name %><% end -%>
 end
