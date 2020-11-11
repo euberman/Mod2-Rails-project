3b130f75eb2539879e4d3baa7bb6dd733bedd45f
Gabbi Nguyen <gabbinguyen@gmail.com>
2020-11-11 15:35:28 -0600

    edit/delete on session

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -15,27 +15,28 @@ class StudentsController < ApplicationController
     @student = Student.new(student_params)
   end
 
-  def edit
-    @student = Student.find(params[:id])
-  end
+  # def edit
+  #   @student = Student.find(params[:id])
+  # end
 
-  def update
-    @student = Student.find(params[:id])
-    @student.update(student_params)
-    redirect_to student_path(@student)
-  end
+  # def update
+  #   @student = Student.find(params[:id])
+  #   @student.update(student_params)
+  #   redirect_to student_path(@student)
+  # end
 
   def destroy
+  end
+  
+  def student_profile
+    @student = Student.find(session[:student_id])
+    # render :layout => 'profile_layout'
   end
 
   private 
   def student_params
     params.require(:student).permit(:cohort_id, :name, :bio)
   end 
-  
-  def student_profile
-    @student = Student.find(session[:student_id])
-    # render :layout => 'profile_layout'
-  end
+
 
 end
