fda5561d03196d95610a13a36f706d7127c510cb
euberman <euberman@gmail.com>
2020-11-11 19:48:58 -0600

    added methods to video and blog models

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,16 @@
 class Blog < ApplicationRecord
   belongs_to :student
   has_many :likes
+
+  def owner_name
+    self.student.name
+  end
+
+  def time_since
+    t_now = Time.now.to_i
+    t_created = Time.new(Blog.first.date).to_i
+    t_diff = (time_now - time_created) / 86400
+    t_diff
+    # time(time_created, time_now)
+  end
 end
